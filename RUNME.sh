0aeaa31b379c7ec0bb8731ef848c1acbb28521d7
frazier <frozen_tearz@163.com>
2017-10-31 23:44:51 +0800

    init

--- RUNME.sh
+++ RUNME.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh   
+wget --no-check-certificate https://github.com/frozentearz/SSR-Custom-Edition/blob/master/shadowsocksR.sh
 chmod +x shadowsocksR.sh
 ./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
 
@@ -8,4 +8,4 @@ wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
 ./bbr.sh
 
-wget --no-check-certificate https://
+reboot
